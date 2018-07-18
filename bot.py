d434d1446ec94b5f988e1fd3a794e451264b6997
kittipat1413 <41315858+kittipat1413@users.noreply.github.com>
2018-07-18 20:43:59 +0700

    echo

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, abort
-#from linebot import (LineBotApi, WebhookHandler)
-#from linebot.exceptions import (InvalidSignatureError)
-#from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
 
-#line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-#handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('X8H8tZp/fwqo7QXQxIYAzB9JFl5Rc7gGC8YGiVvoCMOCo+LXXsGJ49IsLX4x9uL66s7y6m03sbJQDKW9xQ0tTcpztuOdWjF0Z2Shf+CnpwVfYpZovZq/UC7IHQI1qZ8a0Pjx99uhV2Wtnis79+XREAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('ef9543e4b95c520018a463fb79b6e9d9')
 
 @app.route("/")
 def hello():
@@ -14,27 +14,27 @@ def hello():
 
 @app.route("/webhook", methods=['GET','POST'])
 def webhook():
-    # get X-Line-Signature header value
-    #signature = request.headers['X-Line-Signature']
+     get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    #body = request.get_data(as_text=True)
-    #app.logger.info("Request body: " + body)
+     get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    #try:
-    #    handler.handle(body, signature)
-    #except InvalidSignatureError:
-    #    abort(400)
+     handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
     return 'OK'
     
 
-#@handler.add(MessageEvent, message=TextMessage)
-#def handle_message(event):
-#    line_bot_api.reply_message(
-#        event.reply_token,
-#        TextSendMessage(text=event.message.text))
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
