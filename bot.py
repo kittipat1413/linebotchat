d5697c5a805c7c4ab97310d9cd689b12cce8a1f2
kittipat1413 <41315858+kittipat1413@users.noreply.github.com>
2018-07-19 11:20:27 +0700

    work

--- bot.py
+++ bot.py
@@ -33,9 +33,9 @@ def webhook():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text=='Get token':
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.replyToken))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.reply_token))
     elif event.message.text=='Get id':
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.source.userId))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.source.user_id))
     else :
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
