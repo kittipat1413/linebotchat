00785b4d21babf8dd9091055d2bfd9b17a6b44a3
kittipat1413 <41315858+kittipat1413@users.noreply.github.com>
2018-07-19 17:04:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,22 +15,22 @@ def hello():
 @app.route("/webhook", methods=['POST'])
 def webhook():
     # get X-Line-Signature header value
- #   signature = request.headers['X-Line-Signature']
+    signature = request.headers['X-Line-Signature']
 
     # get request body as text
- #   body = request.get_data(as_text=True)
- #   app.logger.info("Request body: " + body)
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
     # handle webhook body
- #   try:
- #       handler.handle(body, signature)
- #   except InvalidSignatureError:
- #       abort(400)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
     return 'OK'
     
 
-#@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text=='Get token':
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.reply_token))
